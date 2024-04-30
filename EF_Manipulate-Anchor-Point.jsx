117d1b21aa29e70b920888ce2c51874f5d935ffe
Eveline Falcão <falcao.eveline@gmail.com>
2024-04-30 10:58:01 -0300

    add comments and rename some variables for clarity

--- EF_Manipulate-Anchor-Point.jsx
+++ EF_Manipulate-Anchor-Point.jsx
@@ -50,7 +50,8 @@ function createUserInterface(thisObj, userInterfaceString, scriptName){
 
     var anchorPointGroup = UI.anchorPointGroup;
     var offsetPositionGroup = UI.offsetPositionGroup;
-
+    // Check the radio button states
+    // Row 1
     for(var c = 0; c < anchorPointGroup.row1.children.length; c++){
         anchorPointGroup.row1.children[c].onClick = function() {
             for (var i = 0; i < anchorPointGroup.row1.children.length; i++) {
@@ -59,7 +60,7 @@ function createUserInterface(thisObj, userInterfaceString, scriptName){
             }
         };
     }
-
+    // Row 2
     for(var c = 0; c < anchorPointGroup.row2.children.length; c++){
         anchorPointGroup.row2.children[c].onClick = function() {
             for (var i = 0; i < anchorPointGroup.row2.children.length; i++) {
@@ -68,7 +69,7 @@ function createUserInterface(thisObj, userInterfaceString, scriptName){
             }
         };
     }
-
+    // Row 3
     for(var c = 0; c < anchorPointGroup.row3.children.length; c++){
         anchorPointGroup.row3.children[c].onClick = function() {
             for (var i = 0; i < anchorPointGroup.row3.children.length; i++) {
@@ -78,6 +79,7 @@ function createUserInterface(thisObj, userInterfaceString, scriptName){
         };
     }
 
+    // Default state for the offset value
     offsetPositionGroup.xText.onChange = function(){
         var xVal = parseFloat(offsetPositionGroup.xText.text);
         if(isNaN(xVal)){
@@ -103,30 +105,35 @@ function createUserInterface(thisObj, userInterfaceString, scriptName){
 function moveAnchorPoint(layers){
     for(var l = 0; l < layers.length; l++){
         var currentLayer = layers[l];
-        var sourceRect = currentLayer.sourceRectAtTime(0, false);
-        var anchorPointProp = currentLayer.property("ADBE Transform Group").property("ADBE Anchor Point");
-        var currentAnchorValue = anchorPointProp.value;
         var positionProp = currentLayer.property("ADBE Transform Group").property("ADBE Position");
+        var initialPositionValue = positionProp.value;
+        var anchorPointProp = currentLayer.property("ADBE Transform Group").property("ADBE Anchor Point");
+        var initialAnchorValue = anchorPointProp.value;
+        var finalAnchorValue;
 
+        // Bounding box
+        var sourceRect = currentLayer.sourceRectAtTime(0, false);
         var top = sourceRect.top;
         var left = sourceRect.left;
         var width = sourceRect.width;
         var height = sourceRect.height;
 
+        // Radio button selection
+        // Row 1
         if (UI.anchorPointGroup.row1.a.value){
             anchorPointProp.setValue([left, top]);
         } else if (UI.anchorPointGroup.row1.b.value){
             anchorPointProp.setValue([left + width / 2, top]);
         } else if (UI.anchorPointGroup.row1.c.value){
             anchorPointProp.setValue([left + width, top]);
-
+        // Row 2
         } else if (UI.anchorPointGroup.row2.a.value){
             anchorPointProp.setValue([left, top + height / 2]);
         } else if (UI.anchorPointGroup.row2.b.value){
             anchorPointProp.setValue([left + width / 2, top + height / 2]);
         } else if (UI.anchorPointGroup.row2.c.value){
             anchorPointProp.setValue([left + width, top + height / 2]);
-
+        // Row 3
         } else if (UI.anchorPointGroup.row3.a.value){
             anchorPointProp.setValue([left, top + height]);
         } else if (UI.anchorPointGroup.row3.b.value){
@@ -134,54 +141,18 @@ function moveAnchorPoint(layers){
         } else if (UI.anchorPointGroup.row3.c.value){
             anchorPointProp.setValue([left + width, top + height]);
         }
-        var positionDif = [anchorPointProp.value[0] - currentAnchorValue[0], anchorPointProp.value[1] - currentAnchorValue[1]]
-        positionProp.setValue([positionProp.value[0] + positionDif[0], positionProp.value[1] + positionDif[1]]);
+        finalAnchorValue = anchorPointProp.value;
+
+        // Move position
+        var distance = [finalAnchorValue[0] - initialAnchorValue[0], finalAnchorValue[1] - initialAnchorValue[1]]; // final anchor point position - initial anchor point position
+        positionProp.setValue([initialPositionValue[0] + distance[0], initialPositionValue[1] + distance[1]]);
     }
 };
 
-// function adjustPosition(layers){
-    // for(var l = 0; l < layers.length; l++){
-        // var currentLayer = layers[l];
-        // var positionProp = currentLayer.property("ADBE Transform Group").property("ADBE Position");
-
-    //     if (UI.anchorPointGroup.row1.a.value){
-    //         positionProp.setValue([left, top]);
-    //     } else if (UI.anchorPointGroup.row1.b.value){
-    //         anchorPointProp.setValue([left + width / 2, top]);
-    //     } else if (UI.anchorPointGroup.row1.c.value){
-    //         anchorPointProp.setValue([left + width, top]);
-
-    //     } else if (UI.anchorPointGroup.row2.a.value){
-    //         anchorPointProp.setValue([left, top + height / 2]);
-    //     } else if (UI.anchorPointGroup.row2.b.value){
-    //         anchorPointProp.setValue([left + width / 2, top + height / 2]);
-    //     } else if (UI.anchorPointGroup.row2.c.value){
-    //         anchorPointProp.setValue([left + width, top + height / 2]);
-
-    //     } else if (UI.anchorPointGroup.row3.a.value){
-    //         anchorPointProp.setValue([left, top + height]);
-    //     } else if (UI.anchorPointGroup.row3.b.value){
-    //         anchorPointProp.setValue([left + width / 2, top + height]);
-    //     } else if (UI.anchorPointGroup.row3.c.value){
-    //         anchorPointProp.setValue([left + width, top + height]);
-    //     }
-    // }
-// };
-
 var comp = app.project.activeItem;
-
 var UI = createUserInterface(this, resourceString, "EF_Manipulate Anchor Point");
-
-// UI.update();
 
 UI.applyButton.onClick = function(){
     var layers = comp.selectedLayers;
     moveAnchorPoint(layers);
 };
-
-
-/*
-
-> test backick `` for strings
-
-*/
